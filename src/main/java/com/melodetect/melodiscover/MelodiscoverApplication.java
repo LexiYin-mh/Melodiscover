08a9302cc01e654a931074cffedb547e6d489948
Lexi Yin <mhyin08@gmail.com>
2023-12-29 16:31:08 -0500

    first commit

---
+++ src/main/java/com/melodetect/melodiscover/MelodiscoverApplication.java
@@ -0,0 +1,13 @@
+package com.melodetect.melodiscover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodiscoverApplication.class, args);
+	}
+
+}
